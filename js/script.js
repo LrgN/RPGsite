daf2a73efc6eee798a30eb17191db4bb563ded8b
abelmark <mabel16113@gmail.com>
2016-02-01 12:00:08 -0800

    intro animation fixed

--- js/script.js
+++ js/script.js
@@ -8,23 +8,59 @@ var killAudio = new Audio('./sounds/kill.wav');
 var mAttack = new Audio('./sounds/somersalt.wav');
 // audio.play();
 
-//Sets beginning of game variables
-	var monsterHP = 20, userHP = 20;
-	var monsterMP = 20, userMP = 20;
-	var userDMG, monsterDMG;
-	var defend, mDefend;
-	var medic = 0; mHeal = 0;
-	var choice;
-	var clicked = false;
+var introText = "On your way to the village of Anselton a monster jumps out of the bush...";
+var splitIntro = introText.split("");
 
-	//animates character coming onto screen
+$('#monster').hide();
+$('#user').hide();
+
+var beginBattle = function(){
+	console.log("hi");
+	$('#user').show();
+	$('#monster').show();
 	$('#monster').animate({	right: "0px", bottom: "-10px"});
 	$('#user').animate({	left: "-40px", bottom: "10px"});
+}
+
+//Intro sequence
+$(splitIntro).each(function(index){
+	var value = $(this);
+	setTimeout( function(){
+		$("#battle-dialogue").append(splitIntro[index]);
+		if(index + 1 === splitIntro.length){
+			beginBattle();
+		}
+	}, 100 * (index + 1));
+})
+
+//Creates character objects
+var monster = {hp: 20,
+								mp: 20,
+								damage: 0,
+								defend: 0,
+								heal: 0,
+								choice: 0};
+var player = {hp: 20,
+							 mp: 20,
+							 damage: 0,
+							 defend: 0,
+							 heal: 0,
+							 choice: "",
+							 clicked: false};
+	
+	// var monsterHP = 20, userHP = 20;
+	// var monsterMP = 20, userMP = 20;
+	// var userDMG, monsterDMG;
+	// var defend, mDefend;
+	// var medic = 0; mHeal = 0;
+	// var choice;
+	// var clicked = false;
+	
 	//animates player character to show that the knight attacked
 	var userAttack = function(){
 	  $('#user').animate({left: "+=30px"}, 'fast')
 	  $('#user').animate({left: "-=30px"}, 'fast')
-	  $('#userDamage').html("<h1>" + monsterDMG + "</h1>").animate({bottom: "+=60px", opacity: "0", color: "red"}, 1000, function(){
+	  $('#userDamage').html("<h1>" + monster.damage + "</h1>").animate({bottom: "+=60px", opacity: "0", color: "red"}, 1000, function(){
 	  	$('#userDamage').html("").css({bottom: "-=60px", opacity: "1"});
 	  });	
 	  
@@ -33,7 +69,7 @@ var mAttack = new Audio('./sounds/somersalt.wav');
 	var monsterAttack = function(){
 		$('#monster').animate({right: "+=30px"}, 'fast')
 		$('#monster').animate({right: "-=30px"}, 'fast')
-		$('#monsterDamage').html("<h1>" + userDMG + "</h1>").animate({bottom: "+=40px", opacity: "0"}, 1000, function(){
+		$('#monsterDamage').html("<h1>" + player.damage + "</h1>").animate({bottom: "+=40px", opacity: "0"}, 1000, function(){
 	  	$('#monsterDamage').html("").css({bottom: "-=40px", opacity: "1"});
 	  });
 	}	
@@ -41,45 +77,43 @@ var mAttack = new Audio('./sounds/somersalt.wav');
 
 	function monsterChoice() {
 		//checks to see if monster has available MP for heal if so it runs
-	  if(monsterMP < 10){
-	    monsterDecision = 1 + (Math.floor(Math.random() * 2));
-	      switch (monsterDecision) {
+	  if(monster.mp == 0){
+	    monster.choice = 1 + (Math.floor(Math.random() * 2));
+	      switch (monster.choice) {
 	      case 1:
-	        monsterDMG = 1 + (Math.floor(Math.random() * 10));
-	        mDefend = 1;
-	        mHeal = 0;
-	        monsterAttack();
+	        monster.damage = 1 + (Math.floor(Math.random() * 10));
+	        monster.defend = 1;
+	        monster.heal = 0;
 	        break;
 	      case 2:
-	        monsterDMG = 0;
-	        mDefend = 2;
-	        mHeal = 0;
+	        monster.damage = 0;
+	        monster.defend = 2;
+	        monster.heal = 0;
 	        break;
 	      };
 	  //runs if monster has available MP to heal
 	  }else{
-		monsterDecision = 1 + (Math.floor(Math.random() * 3));
+		monster.choice = 1 + (Math.floor(Math.random() * 3));
 		
-			switch (monsterDecision) {
+			switch (monster.choice) {
 				//monster attacks
 				case 1:
-					monsterDMG = 1 + (Math.floor(Math.random() * 10));
-					mDefend = 1;
-					mHeal = 0;
-					monsterAttack();
+					monster.damage = 1 + (Math.floor(Math.random() * 10));
+					monster.defend = 1;
+					monster.heal = 0;
 					break;
 				//monster defends
 				case 2:
-					monsterDMG = 0;
-					mDefend = 2;
-					mHeal = 0;
+					monster.damage = 0;
+					monster.defend = 2;
+					monster.heal = 0;
 					break;
 				//monster heals
 				case 3:
-					monsterDMG = 0;
-					mDefend = 1;
-					mHeal = 5;
-					monsterMP -= 10;
+					monster.damage = 0;
+					monster.defend = 1;
+					monster.heal = 5;
+					monster.mp -= 10;
 					break;
 		}	
 	  	}
@@ -88,95 +122,61 @@ var mAttack = new Audio('./sounds/somersalt.wav');
 
 	//Assigns numbers for user choice algorithm based on user selection
 	function userChoice() {
-		switch (choice) {
+		switch (player.choice) {
 			case "attack":
-				userDMG = 1 + (Math.floor(Math.random() * 10));
-				defend = 1;
-				uHeal = 0;
+				player.damage = 1 + (Math.floor(Math.random() * 10));
+				player.defend = 1;
+				player.heal = 0;
 				break;
 			case "defend":
-				userDMG = 0;
-				defend =  2;
-				uHeal = 0;
+				player.damage = 0;
+				player.defend =  2;
+				player.heal = 0;
 				break;
 			case "heal":
-				userDMG = 0;
-				defend = 1;
-				uHeal = 5;
-				userMP -= 10;
+				player.damage = 0;
+				player.defend = 1;
+				player.heal = 5;
+				player.mp -= 10;
 				break;
 		}
 
 	};
 
 	//algorithm to change HP based on user and monster choices
-	function battle() {
-		monsterHP = (monsterHP - (userDMG / mDefend)) + mHeal;
-		userHP = (userHP - (monsterDMG / defend)) + uHeal;
+	function userTurn(){
+		monster.hp = (monster.hp - (user.damage / monster.defend)) + monster.heal;
+		if(player.choice == 'attack'){
+			userAttack();
+			hitAudio.play();
+		}else if(player.choice == 'heal'){
+			healAudio.play();
+		}
 	};
+
+	function monsterTurn(){
+		player.hp = (player.hp - (monster.damage / player.defend)) + player.heal;
+		if(monster.choice == 1){
+			monsterAttack();
+			mAttack.play();
+		}else if(monster.choice == 3){
+			healAudio.play();
+		}
+	}
 
 	//Corrects for HP going below zero
 	function belowZero() {
-	  if(userHP <= 0){
-	    userHP = 0;
+	  if(player.hp <= 0){
+	    player.hp = 0;
 	  }
-	  if(monsterHP <=0){
-	    monsterHP = 0;
+	  if(monster.hp <=0){
+	    monster.hp = 0;
 	  }
 	}
 
-	//Shows dialogue for battle to users
-	function dialogue() {
-
-		if(choice == "attack" && monsterDecision == 1){
-			$('#battle-dialogue').empty().append("You took " + (monsterDMG / defend) + " points of damage.  You inflicted " + (userDMG / mDefend) + " points of damage. You have " + userHP + " HP left.  The monster has " + monsterHP + " HP left.");
-	 		userAttack();
-	 		hitAudio.play();
-		}
-		else if(choice == "attack" && monsterDecision == 2){
-			$('#battle-dialogue').empty().append("You swing your sword at the monster for " + (userDMG/mDefend) + " but he deflects most of your damage. You have " + userHP + " HP left.  The monster has " + monsterHP + " HP left.")
-	    userAttack();
-	    hitAudio.play();
-	  }
-		else if(choice == "attack" && monsterDecision == 3){
-			$('#battle-dialogue').empty().append("You strike the monster for " + userDMG + " HP and the monster heals himself " + mHeal + " HP You have " + userHP + " HP left.  The monster has " + monsterHP + " HP left and " + monsterMP + " MP left.");
-	    userAttack();
-	    hitAudio.play();
-		}
-		else if(choice == "defend" && monsterDecision == 1){
-			$('#battle-dialogue').empty().append("You hold up your shield and the monster strikes you for " + (monsterDMG/defend) + " HP You have " + userHP + " HP left.  The monster has " + monsterHP + " HP left.");
-			monsterAttack();
-			mAttack.play();
-		}
-		else if(choice == "defend" && monsterDecision == 2){
-			$('#battle-dialogue').empty().append("You both curl up in the fetal position like wussies. You have " + userHP + " HP left.  The monster has " + monsterHP + " HP left.");
-		}
-		else if(choice == "defend" && monsterDecision == 3){
-			$('#battle-dialogue').empty().append("You hold up your shield with the monster heals himself " + mHeal + " HP. You have " + userHP + " HP left.  The monster has " + monsterHP + " HP left and " + monsterMP + " MP left.");
-		}
-		else if(choice == "heal" && monsterDecision == 1){
-			$('#battle-dialogue').empty().append("You heal yourself for " + medic + " HP, but the monster attacked you for " + monsterDMG + " You have " + userHP + " HP left. and " + userMP + " MP left.  The monster has " + monsterHP + " HP left.");
-			monsterAttack();
-			healAudio.play();
-		}
-		else if(choice == "heal" && monsterDecision == 2)
-			{
-			$('#battle-dialogue').empty().append("You heal yourself for " + medic + " HP and the monster raises his arms in defense. You have " + userHP + " HP left and " + userMP + " MP left.  The monster has " + monsterHP + " HP left.");
-			healAudio.play();
-			}
-		else if(choice == "heal" && monsterDecision == 3){
-			$('#battle-dialogue').empty().append("You heal yourself for " + medic + " HP while the monster heals himself for " + mHeal + " HP... You have " + userHP + " HP left and " + userMP + " MP left.  The monster has " + monsterHP + " HP left and " + monsterMP + " MP left.");
-			healAudio.play();
-		}
-		else{
-			$('#battle-dialogue').empty().append("Not sure what happened here...");
-		}
-		
-	};
-
 	//Determines if game is over that begins endgame animation
 	function endGame() {
-		if(userHP <= 0){
+		if(player.hp <= 0){
 			$("#user").toggle("explode");
 		  $("#user-menu").hide();
 		  audio.pause();
@@ -186,7 +186,7 @@ var mAttack = new Audio('./sounds/somersalt.wav');
 	    $("#skeleton").animate({right: "200px", height: "400px", width: "400px"});
 	    $("#monster").animate({right: "200px"});
 		}
-		else if(monsterHP <=0){
+		else if(monster.hp <=0){
 	  
 		  $("#monster").toggle('explode');
 			$("#user-menu").hide();
@@ -206,50 +206,58 @@ var mAttack = new Audio('./sounds/somersalt.wav');
 
 	//Event listener for user actions
 	$('#attack').click(function() {
-		choice = "attack";
-		clicked = true;
-		if(clicked == true){
+		player.choice = "attack";
+		player.clicked = true;
+		if(player.clicked == true){
 			monsterChoice();
 			userChoice();
-			battle();
+			userTurn();
+			monsterTurn(function(){
+
+			}, 2000);
 	    belowZero();
-			dialogue();
+			// dialogue();
 			endGame();
-			clicked = false;
+			player.clicked = false;
 		}else{
-			var clicked = false;
+			player.clicked = false;
 		}
 			
 	});
 	$('#defend').click(function() {
-			choice = "defend";
-			clicked = true; 
-	    if(clicked == true){
+			player.choice = "defend";
+			player.clicked = true; 
+	    if(player.clicked == true){
 	    monsterChoice();
 	    userChoice();
-	    battle();
-	    dialogue();
+	    userTurn();
+	    monsterTurn(function(){
+
+	    }, 2000);
+	    belowZero();
+	    // dialogue();
 	    endGame();
-	    clicked = false;
+	    player.clicked = false;
 	  }else{
-	    var clicked = false;
+	    player.clicked = false;
 	  }
 	});
 	$('#heal').click(function() {
-	  if(userMP < 10){
+	  if(player.mp < 10){
 	    alert("Your MP is too low")
 	  }else{
-			choice = "heal"; 
-			clicked = true;
-	      if(clicked == true){
+			player.choice = "heal"; 
+			player.clicked = true;
+	      if(player.clicked == true){
 	        monsterChoice();
 	        userChoice();
-	        battle();
-	        dialogue();
+	        userTurn();
+	        belowZero();
+	        // dialogue();
 	        endGame();
-	        clicked = false;
+	        player.clicked = false;
 	      }else{
-	    var clicked = false;
+	    		player.clicked = false;
 	      }
 	  }
 	});
